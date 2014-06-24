1d75b940fbbe82a342b2785de54757e85d186155
Steve Long <steven989@gmail.com>
2014-06-24 16:07:36 -0400

    hover menu

---
+++ app/assets/javascripts/cosmetics.js
@@ -0,0 +1,21 @@
+$(function(){
+    toggleUserProfileMenu();
+});
+
+
+// unhide profile option menu on hover user name
+
+function toggleUserProfileMenu() {
+
+        $('#user').off('mouseover').on('mouseover',function(){
+                $('.user_options_menu').removeClass('hidden');
+        });
+    
+        $('#user').off('mouseleave').on('mouseleave',function(){
+                $('.user_options_menu').addClass('hidden');
+        });
+        
+        // $('#user').off('click').on('click',function(){
+        //         window.open("/user/profile#summary","_self")
+        // });
+}
